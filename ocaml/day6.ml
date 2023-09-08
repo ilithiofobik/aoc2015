ce9644261864cc6b9e49a9dc86e03681c2334e79
ilithiofobik <wojciech.sek17@gmail.com>
2023-09-09 01:00:27 +0200

    some refactor on day6 in ocaml

--- ocaml/day6.ml
+++ ocaml/day6.ml
@@ -1,4 +1,4 @@
-type commandType =
+type switch =
   | On 
   | Off 
   | Toggle
@@ -6,7 +6,7 @@ type commandType =
 
 type command = 
   {
-    commandType : commandType;
+    commandType : switch;
     start_row : int;
     start_col : int;
     end_row : int;
@@ -20,97 +20,65 @@ let str_to_command s =
     |> String.map (fun c -> match c with | ',' -> ' ' | x -> x) 
     |> String.split_on_char ' '
   in
-  if s |> String.starts_with ~prefix:"turn off " then 
+  let (ct, sr, sc, er, ec) = 
+    if s |> String.starts_with ~prefix:"turn off " then 
+      (Off, 2, 3, 5, 6)
+    else if s |> String.starts_with ~prefix:"turn on " then
+      (On, 2, 3, 5, 6)
+    else
+      (Toggle, 1, 2, 4, 5)
+  in 
     {
-      commandType = Off;
-      start_row = List.nth list  2 |> int_of_string;
-      start_col = List.nth list  3 |> int_of_string;
-      end_row = List.nth list  5 |> int_of_string;
-      end_col = List.nth list  6 |> int_of_string;
-    }
-  else if s |> String.starts_with ~prefix:"turn on " then 
-    {
-      commandType = On;
-      start_row = List.nth list  2 |> int_of_string;
-      start_col = List.nth list  3 |> int_of_string;
-      end_row = List.nth list  5 |> int_of_string;
-      end_col = List.nth list  6 |> int_of_string;
-    }
-  else 
-    {
-      commandType = Toggle;
-      start_row = List.nth list  1 |> int_of_string;
-      start_col = List.nth list  2 |> int_of_string;
-      end_row = List.nth list  4 |> int_of_string;
-      end_col = List.nth list  5 |> int_of_string;
+      commandType = ct;
+      start_row = sr |> List.nth list |> int_of_string;
+      start_col = sc |> List.nth list |> int_of_string;
+      end_row   = er |> List.nth list |> int_of_string;
+      end_col   = ec |> List.nth list |> int_of_string;
     }
 ;;
 
-let cmd_check cmd (row, col) = 
-  cmd.start_row <= row && 
-  row <= cmd.end_row && 
-  cmd.start_col <= col && 
-  col <= cmd.end_col;;
+let size = 1000;;
 
-let cmd_map cmd light = 
+let cmd_map1 cmd light = 
   match cmd.commandType with 
-  | On -> true 
-  | Off  -> false 
-  | Toggle -> not light;;
+  | On -> 1L 
+  | Off  -> 0L 
+  | Toggle -> Int64.sub 1L light;;
 
-let task1 lines =
-  let lights = Array.make_matrix 1000 1000 false in 
-  let index_vec = List.init 1000 (fun n -> n) in 
-  let index_matrix = Utils.list_cartesian index_vec index_vec in 
-
-  lines 
-  |> List.map str_to_command
-  |> List.iter (fun cmd ->
-    index_matrix 
-    |> List.filter (cmd_check cmd)
-    |> List.iter (fun (row, col) ->
-        lights.(row).(col) <- cmd_map cmd lights.(row).(col)
-      )
-    );
-
-  index_matrix
-  |> List.filter (fun (row, col) -> lights.(row).(col))
-  |> List.length  
-  ;;
-  
 let cmd_map2 cmd light = 
   match cmd.commandType with 
-  | On -> light + 1 
-  | Off  -> max (light - 1) 0 
-  | Toggle -> light + 2
+  | On -> Int64.add light 1L
+  | Off  -> Int64.sub light 1L |> Int64.max 0L
+  | Toggle -> Int64.add light 2L
   ;;
 
-let task2 lines =
-  let lights = Array.make_matrix 1000 1000 0 in 
-  let index_vec = List.init 1000 (fun n -> n) in 
-  let index_matrix = Utils.list_cartesian index_vec index_vec in 
-  let index_matrix_seq = List.to_seq index_matrix in
+let task cmd_map lines =
+  let lights = Array.make_matrix size size 0L in 
+  let index_vec = Array.init size Fun.id |> Array.to_seq in
 
   lines 
   |> List.map str_to_command
   |> List.iter (fun cmd ->
-    index_matrix 
-    |> List.filter (cmd_check cmd)
-    |> List.iter (fun (row, col) ->
-        lights.(row).(col) <- cmd_map2 cmd lights.(row).(col)
-      )
-    );
-
-  index_matrix_seq
-  |> Seq.map (fun (row, col) -> lights.(row).(col))
-  |> Seq.map Int64.of_int
-  |> Seq.fold_left Int64.add Int64.zero
+    for row = cmd.start_row to cmd.end_row do
+      for col = cmd.start_col to cmd.end_col do
+        lights.(row).(col) <- cmd_map cmd lights.(row).(col)
+      done    
+    done
+  );
+  
+  index_vec
+  |> Seq.map (fun row ->
+    index_vec
+    |> Seq.map (fun col -> lights.(row).(col))
+    |> Utils.seq_sum64
+  )
+  |> Utils.seq_sum64
   ;;
 
 let lines = Utils.file_to_lines "../input/day6.txt" in
-let result1 = task1 lines in
-let result2 = task2 lines in
-  Printf.printf "Task1: %d\n" result1;
+let result1 = task cmd_map1 lines in
+let result2 = task cmd_map2 lines in
+  Printf.printf "Task1: %Ld\n" result1;
   Printf.printf "Task2: %Ld\n" result2;
-  assert (result1 = 400410); 
+  assert (result1 = 400410L); 
   assert (result2 = 15343601L); 
